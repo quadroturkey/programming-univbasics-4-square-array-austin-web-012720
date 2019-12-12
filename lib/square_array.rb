4e72e90bc95ddf9bc6d3dc7cd88d0b31edfd838f
Reese M Bowman <reesemb1@gmail.com>
2019-12-12 00:35:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   new_array = []
   
   while i < array.length
-    squared = array[i] ^ 2 
+    squared = array[i] * array[i] 
     new_array.push(squared)
     i += 1 
   end
